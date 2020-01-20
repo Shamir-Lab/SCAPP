1f26abcaced98e0e6bfc64f9b5117fde26b45474
David Pellow <dpellow@localhost.localdomain>
2020-01-20 14:17:35 +0200

    Reverse last changes

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
@@ -17,13 +17,10 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    scripts = ['scapp.py','recycle.py', 'make_fasta_from_fastg.py', 'classify_fastg.py',\
-                'find_plasmid_gene_matches.py', 'parse_plasmid_scores.py', \
-                'create_hits_fasta.py'],
-    #scripts = ['scapp/bin/scapp.py','scapp/bin/recycle.py', 'scapp/bin/make_fasta_from_fastg.py', 'scapp/bin/classify_fastg.py',\
-#                'scapp/bin/find_plasmid_gene_matches.py', 'scapp/bin/parse_plasmid_scores.py', \
-#                'scapp/bin/create_hits_fasta.py'],
-    packages = find_packages("scapp"),#['scapp'],#'scapp.recyclelib'],
+    scripts = ['scapp/bin/scapp.py','scapp/bin/recycle.py', 'scapp/bin/make_fasta_from_fastg.py', 'scapp/bin/classify_fastg.py',\
+                'scapp/bin/find_plasmid_gene_matches.py', 'scapp/bin/parse_plasmid_scores.py', \
+                'scapp/bin/create_hits_fasta.py'],
+    packages = ['scapp'],#'scapp.recyclelib'],
     install_requires=[
         'networkx==2.4',
         'pysam==0.15.3',
